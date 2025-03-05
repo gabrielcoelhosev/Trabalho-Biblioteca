1f4f48be79c15273f6c5d286b0fb979446d9485c
gabrielcoelhosev <gabrielcoelhosev.dev@gmail.com>
2025-03-05 16:50:47 -0300

    commit inicial

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿public class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Digite seu nome: ");
+        string nome = Console.ReadLine();
+        Console.WriteLine($"Olá {nome}!");
+    }
+}
