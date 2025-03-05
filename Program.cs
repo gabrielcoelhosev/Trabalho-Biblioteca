cc6ae2db06cb4c0905a44cc2205b652cbaf6d299
gabrielcoelhosev <gabrielcoelhosev.dev@gmail.com>
2025-03-05 18:51:11 -0300

    fix: Classe autor deletada

--- Program.cs
+++ Program.cs
@@ -4,25 +4,25 @@ public class Program
 {
     private static void Main(string[] args)
     {
-        Livro livro1 = new Livro(
-            "Harry Potter",
-            "E a pedra filosofal",
-            "J.K Rowling",
-            "Rocco",
-            "Fantasia",
-            1997,
-            "Dura",
-            208
-        );
+    //     Livro livro1 = new Livro(
+    //         "Harry Potter",
+    //         "E a pedra filosofal",
+    //         "J.K Rowling",
+    //         "Rocco",
+    //         "Fantasia",
+    //         1997,
+    //         "Dura",
+    //         208
+    //     );
         
-        Leitor leitor1 = new Leitor(
-            "Gabriel",
-            10519810936,
-            "Masculino",
-            988955199,
-            "gabrielcoelhosev.dev@gmail.com"
-        );
+    //     Leitor leitor1 = new Leitor(
+    //         "Gabriel",
+    //         10519810936,
+    //         "Masculino",
+    //         988955199,
+    //         "gabrielcoelhosev.dev@gmail.com"
+    //     );
 
-        Console.WriteLine(livro1.Titulo);
+    //     Console.WriteLine(livro1.Titulo);
     }
 }
