b2ed801bf4eb6742051545f763a4e0db89f8cc9e
gabrielcoelhosev <gabrielcoelhosev.dev@gmail.com>
2025-03-07 21:38:16 -0300

    Fix: Corrigindo a Home

--- Program.cs
+++ Program.cs
@@ -1,7 +1,32 @@
-﻿public class Program
+﻿using System.Numerics;
+
+public class Program
 {
     public static void Main(string[] args)
     {       
-        Console.WriteLine("Hello World");
+        while (true)
+        {
+            Console.WriteLine("Bem Vindo a Bibliotéca do Zé!");
+            Console.WriteLine("Digite 1 para cadastrar um leitor | 2 para listar os livros de um leitor | 3 para deletar o livro de um leitor | 4 para sair");
+            var Resposta = Console.ReadLine();
+
+            switch (Resposta)
+            {
+                case "1":
+                    //CadastrarLeitor();
+                    break;
+                case "2":
+                    //ListarLivros();
+                    break;
+                case "3":
+                   //DeletarLivro();
+                   break;
+                case "4":
+                    return;
+                default:
+                    Console.WriteLine("Saindo...");
+                    break; 
+            }   
+        }
     }
 }
