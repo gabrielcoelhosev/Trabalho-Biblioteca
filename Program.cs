53f33ade6efeae4e6adf1d0bf25d47390e21a2c0
leandro jader <ldandro>
2025-03-11 10:31:16 -0300

    aplicando refatoração de codigo

--- Program.cs
+++ Program.cs
@@ -8,9 +8,8 @@ public class Program
         {
             Console.WriteLine("\t \t \t \t \t \t Bem Vindo a Bibliotéca do Zé!");
             Console.WriteLine("==================================================================================================================");
-            Console.WriteLine("Digite 1 para cadastrar um leitor \t 2 para listar os leitores \t 3 cadastrar um livro \t 4 para listar os livros");
-            Console.WriteLine("");
-            Console.WriteLine("Digite 5 para vincular um livro a um leitor \n 6 para desvincular o livro a um leitor \n 7 para doar um livro \n 8 para sair");
+            Console.WriteLine("Digite 1 para cadastrar um leitor \n 2 para listar os leitores \n 3 cadastrar um livro \n 4 para listar os livros");
+            Console.Write("Digite 5 para vincular um livro a um leitor \n 6 para desvincular o livro a um leitor \n 7 para doar um livro \n 8 para sair");
             var Resposta = Console.ReadLine();
 
             switch (Resposta)
